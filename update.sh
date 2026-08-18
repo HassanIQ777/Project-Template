f87f9a19f2b1a4b81a0cce535a0fe48f732597ec
HassanIQ777 <hassaniq793@gmail.com>
2026-08-18 17:36:55 +0300

    meow

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# WARNING:
+# This hard resets any project in which you run this
+# Use this to update any program and its submodules to their latest versions
+
+echo " ----- Started Updater ----- "
+echo "Updating the program..."
+if git fetch origin && git reset --hard origin/main && git submodule update --init --recursive; then
+    echo "Successfully finished updating!"
+else
+    echo "Failed to update."
+fi
